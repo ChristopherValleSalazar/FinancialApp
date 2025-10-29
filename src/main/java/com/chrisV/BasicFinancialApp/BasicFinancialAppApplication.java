70e4ebd27d3c732c164207f31b0809eb61a6bf79
chris Valle(Linux) <christophervalle3338@gmail.com>
2025-10-28 23:31:17 -0700

    first commit

---
+++ src/main/java/com/chrisV/BasicFinancialApp/BasicFinancialAppApplication.java
@@ -0,0 +1,13 @@
+package com.chrisV.BasicFinancialApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicFinancialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicFinancialAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrisV/BasicFinancialApp/BasicFinancialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisV.BasicFinancialApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicFinancialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
