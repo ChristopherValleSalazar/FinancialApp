f31050c0355b03b38d2848c86384d45be842935c
chris Valle(Linux) <christophervalle3338@gmail.com>
2025-10-28 23:52:06 -0700

    add dependencies and incorporate env file

--- src/main/java/com/chrisV/BasicFinancialApp/BasicFinancialAppApplication.java
+++ src/main/java/com/chrisV/BasicFinancialApp/BasicFinancialAppApplication.java
@@ -8,6 +8,7 @@ public class BasicFinancialAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicFinancialAppApplication.class, args);
+        System.out.println("The Basic Financial App is running!");
 	}
 
 }
