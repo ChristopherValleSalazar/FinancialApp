8b936988a32d4a0e8c7ecb9d60f8485d72810e7e
chris Valle(Linux) <christophervalle3338@gmail.com>
2025-11-08 14:56:02 -0800

    Improve bidirectional relation between Accounts and User

--- src/main/java/com/chrisV/BasicFinancialApp/model/Account.java
+++ src/main/java/com/chrisV/BasicFinancialApp/model/Account.java
@@ -1,5 +1,6 @@
 package com.chrisV.BasicFinancialApp.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,7 +26,6 @@ public class Account {
     //handle in frontend later
     private String currency;
 
-    @Column(unique = true)
     private String bankName;
 
     @Column(unique = true)
@@ -33,10 +33,13 @@ public class Account {
 
     @Column(unique = true)
     private Long routingNumber;
+
     private String notes;
     private String nickname;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
     private User user;
 
     @Column(nullable = false)
--- src/main/java/com/chrisV/BasicFinancialApp/model/User.java
+++ src/main/java/com/chrisV/BasicFinancialApp/model/User.java
@@ -1,8 +1,10 @@
 package com.chrisV.BasicFinancialApp.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -28,7 +30,13 @@ public class User {
     @Column(unique = true)
     private String email;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<Account> accounts;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Account> accounts = new ArrayList<>();
 
+    // helper method (optional but recommended)
+    public void addAccount(Account account) {
+        accounts.add(account);
+        account.setUser(this);
+    }
 }
