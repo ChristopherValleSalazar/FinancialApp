cc7539b5b34b1059b451b6ad728d1be1f7ef6049
chris Valle(Linux) <christophervalle3338@gmail.com>
2025-12-26 18:14:16 -0800

    Create new DTO for updating account endpoint

--- src/main/java/com/chrisV/BasicFinancialApp/service/AccountService.java
+++ src/main/java/com/chrisV/BasicFinancialApp/service/AccountService.java
@@ -3,21 +3,17 @@ package com.chrisV.BasicFinancialApp.service;
 import com.chrisV.BasicFinancialApp.dto.account.AccountRequestDTO;
 import com.chrisV.BasicFinancialApp.dto.account.AccountResponseDTO;
 import com.chrisV.BasicFinancialApp.dto.account.AccountUpdateRequestDTO;
-import com.chrisV.BasicFinancialApp.dto.account.CheckingAccountResponse;
 import com.chrisV.BasicFinancialApp.mapper.AccountMapper;
 import com.chrisV.BasicFinancialApp.model.Account;
 import com.chrisV.BasicFinancialApp.model.AccountType;
-import com.chrisV.BasicFinancialApp.model.CheckingAccountDetails;
 import com.chrisV.BasicFinancialApp.model.User;
 import com.chrisV.BasicFinancialApp.repository.AccountRepo;
-import com.chrisV.BasicFinancialApp.repository.CheckingAccountRepo;
 import com.chrisV.BasicFinancialApp.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -51,24 +47,21 @@ public class AccountService {
     }
 
     private AccountResponseDTO toDto(Account a) {
-        AccountResponseDTO dto = accountMapper.fromEntityToResponseDTO(a);
-        return dto;
+        return accountMapper.fromEntityToResponseDTO(a);
     }
 
     @Transactional(readOnly = true)
     public List<AccountResponseDTO> getAllCheckingAccountsByUserId(Long userId) {
-        List<AccountResponseDTO> accounts = accountRepo.findAllByUserId(userId)
+        return accountRepo.findAllByUserId(userId)
                 .stream()
                 .filter(dto -> dto.getAccountType() == AccountType.CHECKING)
                 .map(account -> accountMapper.fromEntityToResponseDTO(account))
                 .toList();
-        return accounts;
 
     }
 
     @Transactional
     public AccountResponseDTO deleteAccount(Long userId, Long accountId) {
-        User user = repo.findById(userId).orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
         Account account = accountRepo.findById(accountId).orElseThrow(() -> new RuntimeException("Account not found with id: " + accountId));
 
         if (account != null && account.getUser().getId().equals(userId)) {
