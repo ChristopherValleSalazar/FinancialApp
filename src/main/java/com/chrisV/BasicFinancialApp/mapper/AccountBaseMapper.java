21b80035edd6a97da62ecb271b555e98bdbc8a72
chris Valle(Linux) <christophervalle3338@gmail.com>
2025-12-29 19:30:29 -0800

    Make mapper class for future expansion where simpler Dto's are needed

--- src/main/java/com/chrisV/BasicFinancialApp/mapper/AccountBaseMapper.java
+++ src/main/java/com/chrisV/BasicFinancialApp/mapper/AccountBaseMapper.java
@@ -9,6 +9,9 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface AccountBaseMapper {
 
+    //TODO:Make this class for DTO's that only have the base fields or to modify this into different methods for multiple endpoints
+    // for a simple mapping with the ignore = true property to skip unique fields mapping
+
     // Ignoring accountDetails (unique fields per account type), only mapping base fields for ALL account types
     @Mapping(target = "accountDetails", ignore = true)
     AccountResponseDTO accountToAccountResponseDTOWithoutDetails(Account account);
