1b14f81e0add5faa9d9cfd17d642a7e36d7a9ea9
chris Valle(Linux) <christophervalle3338@gmail.com>
2026-01-04 19:21:13 -0800

    Create ENUM for transaction categories

---
+++ src/main/java/com/chrisV/BasicFinancialApp/model/transaction/TransactionCategory.java
@@ -0,0 +1,12 @@
+package com.chrisV.BasicFinancialApp.model.transaction;
+
+public enum TransactionCategory {
+    GROCERIES,
+    UTILITIES,
+    ENTERTAINMENT,
+    TRANSPORTATION,
+    OTHER
+
+
+
+}
